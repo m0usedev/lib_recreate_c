bdd518278e0c1c4ae0027b7ff3f60b0da775c491
Alvaro Joaquin Sobrino Fraile <asobrino@c3r13s3.42madrid.com>
2025-04-08 13:36:10 +0200

    add test ft_isdigit and add divisor

--- main.c
+++ main.c
@@ -6,12 +6,19 @@
 /*   By: asobrino <asobrino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 10:58:58 by asobrino          #+#    #+#             */
-/*   Updated: 2025/04/08 12:53:18 by asobrino         ###   ########.fr       */
+/*   Updated: 2025/04/08 13:33:58 by asobrino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <ctype.h>
 #include <stdio.h>
+
+void	print_divisor_title(char *title)
+{
+	printf("------------------------------\n");
+	printf("%s\n", title);
+}
 
 void	ft_isalpha_test(void)
 {
@@ -20,8 +27,7 @@ void	ft_isalpha_test(void)
 
 	l_may = 'A';
 	l_min = 'a';
-	printf("------------------------------\n");
-	printf("ft_isalpha\n");
+	print_divisor_title("ft_isalpha");
 	while (l_min <= 'z')
 	{
 		printf("Mayus: %c - %i | Minuscula: %c - %i\n", l_may,
@@ -32,8 +38,23 @@ void	ft_isalpha_test(void)
 	printf("Un caracter no alfabetico: 3 - %i\n", ft_isalpha('3'));
 }
 
+void	ft_isdigit_test(void)
+{
+	char	num;
+
+	num = '0';
+	print_divisor_title("ft_isdigit");
+	while (num <= '9')
+	{
+		printf("Numero: %c - %i\n", num, ft_isdigit(num));
+		num++;
+	}
+	printf("Un caracter no alfabetico: D - %i\n", ft_isdigit('D'));
+}
+
 int	main(void)
 {
-	ft_isalpha_test();
+	// ft_isalpha_test();
+	// ft_isdigit_test();
 	return (0);
 }
