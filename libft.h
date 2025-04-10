8948e662bc26bca0f5534ccc2341877454bf6509
Alvaro Joaquin Sobrino Fraile <asobrino@c3r13s6.42madrid.com>
2025-04-10 15:49:47 +0200

    modify ft_isascii and add fr_bzero in libft

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: asobrino <asobrino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 09:59:48 by asobrino          #+#    #+#             */
-/*   Updated: 2025/04/10 13:59:54 by asobrino         ###   ########.fr       */
+/*   Updated: 2025/04/10 15:47:23 by asobrino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,10 @@
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isalnum(int c);
-int		ft_isascii(unsigned char c);
+int		ft_isascii(int c);
 int		ft_isprint(int c);
 size_t	ft_strlen(const char *s);
 void	*ft_memset(void *s, int c, size_t n);
+void	ft_bzero(void *s, size_t n);
 
 #endif
