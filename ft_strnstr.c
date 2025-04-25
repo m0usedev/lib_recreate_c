ee7e144859c0aedcc29f4430b731c9a87561959d
m0usedev <alvaro.sobrino.coding@gmail.com>
2025-04-25 23:12:36 +0200

    norminette ft_strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: asobrino <asobrino@student.42.fr>          +#+  +:+       +#+        */
+/*   By: alvaro-pc <alvaro-pc@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 16:51:50 by asobrino          #+#    #+#             */
-/*   Updated: 2025/04/15 18:01:19 by asobrino         ###   ########.fr       */
+/*   Updated: 2025/04/25 22:39:22 by alvaro-pc        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	{
 		j = 0;
 		while (little[j] && big[i + j] && i + j < len && big[i
-			+ j] == little[j])
+				+ j] == little[j])
 			j++;
 		if (!little[j])
 			return ((char *)(big + i));
