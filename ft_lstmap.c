50a1af81a080c27dff3363ae328452a3e7420f9a
Alvaro Joaquin Sobrino Fraile <asobrino@c2r9s3.42madrid.com>
2025-05-01 11:17:17 +0200

    fix: ft_lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: asobrino <asobrino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/27 15:47:44 by asobrino          #+#    #+#             */
-/*   Updated: 2025/04/27 16:54:48 by asobrino         ###   ########.fr       */
+/*   Updated: 2025/05/01 11:17:00 by asobrino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,18 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new;
 	t_list	*node;
+	void	*content;
 
 	if (!lst || !f || !del)
 		return (NULL);
 	new = NULL;
 	while (lst)
 	{
-		node = ft_lstnew(f(lst->content));
+		content = f(lst->content);
+		node = ft_lstnew(content);
 		if (!node)
 		{
+			del(content);
 			ft_lstclear(&new, del);
 			return (NULL);
 		}
